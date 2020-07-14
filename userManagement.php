39318c54b6bd837e48be8a5dcfd25900315042d2
Michele Corlito <mcorlito.business@gmail.com>
2020-07-14 16:32:16 +0200

    Just some more stuff.

--- userManagement.php
+++ userManagement.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+
+    <h3> User Management </h3>
+
+</head>
+
 <body>
 
 <?php
@@ -11,12 +18,20 @@ $db = new PDO('mysql:host=localhost;dbname=app', 'app', 'app');
 $query = $db->query("SELECT * FROM user");
 $user = $query->fetchAll(\PDO::FETCH_ASSOC);
 
-foreach ($user as $users){
-    $username = $users['username'];
-    echo"$username <br \>";
-}
-
 ?>
+
+<div>
+
+    <?php
+    foreach ($user as $users): ?>
+        <li>
+            <a href="deleteUser.php?id=<?php echo $users['id']; ?>">Delete this user</a><?php
+            echo "  id: " . $users['id'], " ", $users['First_name'], " ", $users['Last_name'], " ", $users['username'], " ", $users['eMail_adress'] ?>
+        </li>
+    <?php
+    endforeach; ?>
+
+</div>
 
 </body>
 </html>
