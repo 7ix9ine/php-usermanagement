9488733abb8db80285f30cc677245011faa70ab2
Michele Corlito <mcorlito.business@gmail.com>
2020-07-14 14:30:51 +0200

    Updated users.php with more information about Dummy User Accounts.

--- users.php
+++ users.php
@@ -4,22 +4,37 @@
 
 $users = [
     [
+        'firstName' => 'John',
+        'lastName' => 'Doe',
+        'emailAdress' => 'johndoe84@outlook.com',
         'username' => 'jdoe',
         'password' => 'jdoe'
     ],
     [
+        'firstName' => 'Benjamin',
+        'lastName' => 'Dover',
+        'emailAdress' => 'ben.dover@gmail.com',
         'username' => 'bdover',
         'password' => 'bdover'
     ],
     [
+        'firstName' => 'Jack',
+        'lastName' => 'Sparrow',
+        'emailAdress' => 'j.sparrow@outlook.com',
         'username' => 'jsparrow',
         'password' => 'jsparrow'
     ],
     [
+        'firstName' => 'Luise',
+        'lastName' => 'Wheeze',
+        'emailAdress' => 'Luise.W@web.de',
         'username' => 'lizzy',
         'password' => 'lizzy'
     ],
     [
+        'firstName' => 'Daisy',
+        'lastName' => 'Lazy',
+        'emailAdress' => 'LazyDaisy@outlook.com',
         'username' => 'dizzy',
         'password' => 'dizzy'
     ]
